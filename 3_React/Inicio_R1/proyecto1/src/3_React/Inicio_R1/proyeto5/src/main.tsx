68ac1441f198d820773b5b35ab0209d2eb2410cd
Banegas , Agustina <tomatecherru@gmail.com>
2026-08-10 20:10:03 -0300

    Delete 3_React/Inicio_R1/proyecto1/src directory

--- 3_React/Inicio_R1/proyecto1/src/3_React/Inicio_R1/proyeto5/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-// App es el componente principal que se inserta en la página.
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
